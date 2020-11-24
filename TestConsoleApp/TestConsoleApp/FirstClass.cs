ce078b5513852d355dd5744fe570806d00c1720a
amohanakrishnan <amohanakrishnan@gmail.com>
2020-11-24 19:55:17 +0530

    First Class

---
+++ TestConsoleApp/TestConsoleApp/FirstClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestConsoleApp
+{
+    class FirstClass
+    {
+        public void TestMethod()
+        {
+            Console.WriteLine("Test Method");
+        }
+    }
+}
